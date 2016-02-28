ca14e2a9e90cacb4d49eb34c40a38276b7505129
SIRprise <peter3@turboprinz.de>
2016-02-28 17:31:22 +0100

    1st

---
+++ GUISynchronisationSingleton/CHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace GUISynchronisationSingleton
+{
+    class CHelper
+    {
+        //lokale Events in öffentlichen EventHandler weitergeben
+        public event EventHandler<TimerNewDataEventArgs> NewData;
+
+        #region Variablen
+        private static volatile CHelper instanzvar;
+        private static object syncRoot = new Object();
+
+        private System.Timers.Timer t1;
+        private const int iTimerIntervall = 250; //in Millisekunden, also delay für "manuelles" Anstoßen der Berechnung
+
+        private static Form1 form1;
+        #endregion
+
+        private CHelper()
+        {
+            t1 = new System.Timers.Timer(iTimerIntervall);
+            t1.Elapsed += new ElapsedEventHandler(OnTimedEvent); // Eventhandler ezeugen der beim Timerablauf aufgerufen wird
+            t1.Start();
+        }
+
+        public static CHelper getInstance(Form1 f1)
+        {
+            form1 = f1;
+            if (instanzvar == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instanzvar == null)
+                        instanzvar = new CHelper();
+                }
+            }
+            return instanzvar;
+        }
+
+        public static CHelper getInstance()
+        {
+            if (instanzvar == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instanzvar == null)
+                        instanzvar = new CHelper();
+                }
+            }
+            return instanzvar;
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            //updateBar
+            if (form1.InvokeRequired) //aktueller Thread GUI-Thread? (Invoke = "anrufen")
+            {
+                if (form1.IsHandleCreated && !form1.IsDisposed)
+                { //soll Absturz beim Beenden verhindern / Invoke durch BeginInvoke ersetzt (BeginInvoke blockiert nicht)
+
+
+                    //Wähle eine Variante:
+
+                    //form1.BeginInvoke(new EventHandler(form1.updateBar2)); //einfachste Version
+                    //form1.BeginInvoke(new EventHandler<TimerNewDataEventArgs>(form1.updateBar3), this, new TimerNewDataEventArgs(1234)); //mit Datenübergabe
+                    NewData(this, new TimerNewDataEventArgs(1234)); //elegant als Event, welches sich selbst kümmert + Datenübergabe
+                }
+                return;
+            }
+        }
+
+    }
+}
---
+++ GUISynchronisationSingleton/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUISynchronisationSingleton
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            CHelper helper = CHelper.getInstance(this);
+            helper.NewData += this.updateBar4;
+        }
+
+        public void updateBar2(object sender, EventArgs e) //Aufruf durch anderen Thread
+        {
+            if(progressBar1.Maximum > progressBar1.Value)
+                progressBar1.Value += 1;
+        }
+
+        public void updateBar3(object sender, TimerNewDataEventArgs e) //Aufruf durch anderen Thread
+        {
+            int zusatzdaten = e.timeMs;
+
+            if (progressBar1.Maximum > progressBar1.Value)
+                progressBar1.Value += 1;
+        }
+
+        public void updateBar4(object sender, TimerNewDataEventArgs e) //Aufruf durch anderen Thread
+        {
+            int zusatzdaten = e.timeMs;
+
+            if (InvokeRequired) //aktueller Thread GUI-Thread? (Invoke = "anrufen")
+            {
+                if (IsHandleCreated && !IsDisposed)
+                { //soll Absturz beim Beenden verhindern
+                    //Invoke durch BeginInvoke ersetzt (BeginInvoke blockiert nicht)
+                    BeginInvoke(new EventHandler<TimerNewDataEventArgs>(updateBar4),sender,e); //ruf dich selbst ggf. nochmal im richtigen Thread auf
+                }
+                return;
+            }
+
+            if (progressBar1.Maximum > progressBar1.Value)
+                progressBar1.Value += 1;
+        }
+        
+    }
+    
+    public class TimerNewDataEventArgs : EventArgs
+    {
+        public readonly int timeMs;
+
+        public TimerNewDataEventArgs(int timeMs)
+        {
+            this.timeMs = timeMs;
+        }
+    }
+    
+    
+}
