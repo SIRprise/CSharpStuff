69eec3f3bb17576d46f74d7445875274c88ce9bd
SIRprise <peter3@turboprinz.de>
2018-04-29 16:52:17 +0200

    added RLE

---
+++ RLEexperiment/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLEexperiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const bool debugOn = false;
+            const int arraysize = 250 * 4;
+            byte[] data = new byte[arraysize];
+
+            //create some random data + fill some parts with same value
+            Random rnd = new Random();
+            rnd.NextBytes(data);
+            for(int i=0; i<5; i++)
+            {
+                int idx = rnd.Next(0,arraysize);
+                int fillsize = rnd.Next(1, data.Length);
+                fillsize = ((fillsize + idx) > arraysize) ? (arraysize - idx) : fillsize; //check upper boarder while filling
+                Enumerable.Repeat(data[idx], fillsize).ToArray().CopyTo(data, idx);
+            }
+             
+            //Enumerable.Repeat((byte)0xAA, arraysize).ToArray().CopyTo(data, 0);
+
+            //encode
+            byte[] dataEnc = RLE.Encode(data);
+
+            //decode
+            byte[] dataDec = RLE.Decode(dataEnc);
+
+            //compare
+            if (dataDec.SequenceEqual(data))
+            {
+                Console.WriteLine("It works! \r\noriginal size\t {0} B,\r\n encoded size\t {1} B", data.Length, dataEnc.Length);
+                if (debugOn)
+                {
+                    Console.WriteLine("\r\nOrg:");
+                    Console.WriteLine(BitConverter.ToString(data));
+                    Console.WriteLine("\r\nEncoded:");
+                    Console.WriteLine(BitConverter.ToString(dataEnc));//.Replace("-", ""));
+                }
+            }
+            else
+            {
+                Console.WriteLine("There is something wrong with your algorithm");
+                if (debugOn)
+                {
+                    Console.WriteLine("\r\nOrg:");
+                    Console.WriteLine(BitConverter.ToString(data));
+                    Console.WriteLine("\r\nEncoded:");
+                    Console.WriteLine(BitConverter.ToString(dataEnc));
+                    Console.WriteLine("\r\nDecoded:");
+                    Console.WriteLine(BitConverter.ToString(dataEnc));
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RLEexperiment/RLE.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLEexperiment
+{
+    public static class RLE
+    {
+        private static byte escape = 254;
+        private static byte maxLen = 250; //must be smaller than escape
+        private static byte threshhold = 4; //start encoding after 4 equal elements
+
+        public static byte[] Encode(byte[] source)
+        {
+            List<byte> encList = new List<byte>();
+            byte runLength = 0;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (source[i] != escape)
+                {
+                    runLength++;
+                    if (runLength >= maxLen)
+                    {
+                        encList.Add(escape);
+                        encList.Add(source[i]);
+                        encList.Add(runLength);
+                        runLength = 0;
+                    }
+                    else
+                    {
+                        if ((i == source.Length-1) || (source[i] != source[i + 1]))
+                        {
+                            if (runLength < threshhold)
+                            {
+                                for(int j=0;j<runLength;j++)
+                                    encList.Add(source[i]);
+                            }
+                            else
+                            {
+                                encList.Add(escape);
+                                encList.Add(source[i]);
+                                encList.Add(runLength);
+                            }
+                            runLength = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    //escape symbol cannot be compressed
+                    encList.Add(escape);
+                    encList.Add(source[i]);
+                }
+            }
+
+
+            return encList.ToArray();
+        }
+
+        public static byte[] Decode(byte[] source)
+        {
+            List<byte> dec = new List<byte>();
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                //case of last char == ctrl char and no byte is following (illegal encoding) is not covered -> exception
+                if(source[i]==escape)
+                {
+                    if (source[i + 1] == escape)
+                    {
+                        dec.Add(escape);
+                        i++;
+                    }
+                    else
+                    {
+                        for (int j = 0; j < source[i + 2]; j++)
+                            dec.Add(source[i+1]);
+                        i += 2;
+                    }
+                }
+                else
+                {
+                    dec.Add(source[i]);
+                }
+            }
+            return dec.ToArray();
+        }
+
+    }
+}
